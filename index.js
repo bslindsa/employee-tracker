a24d21b79cad363889dec5aa7f7f3edd5a8754fb
Brad Lindsay <bslindsa@gmail.com>
2022-01-18 11:05:23 -0700

    Added db.query statements to add departments, roles, and employees.

--- index.js
+++ index.js
@@ -1,4 +1,103 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'CSanderson1!',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+
+
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the department?',
+                name: 'department',
+            }
+        ])
+        .then((data) => {
+            // Add department to department table.
+            db.query(`INSERT INTO departments (department) 
+                VALUE (${data.department});`);
+        });
+};
+
+const addRole = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the role?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'What is the salary of the role?',
+                name: 'salary',
+            },
+            {
+                type: 'list',
+                message: 'Which department does the role belong to?',
+                name: 'department',
+                choices: [...departments], 
+            }
+        ])
+        .then((data) => {
+            // Add role to roles table
+            db.query(`INSERT INTO roles (title, salary, department_id) 
+                VALUE   (${data.title}),
+                        (${data.salary}),
+                        (${data.department});`);
+        });
+
+};
+
+const addEmployee = () => {
+    inquirer
+        prompt([
+            {
+                type: 'input',
+                message: "What is the employee's first name?",
+                name: 'firstName',
+            },
+            {
+                type: 'input',
+                message: "What is the employee's last name?",
+                name: 'lastName',
+            },
+            {
+                type: 'list',
+                message: "What is the employee's role?",
+                name: 'role',
+                choices: [...roles],
+            },
+            {
+                type: 'list',
+                message: "Who is the employee's manager?",
+                name: 'manager',
+                choices: [...employees]
+            }
+        ])
+        .then((data) => {
+            // Add employee data to employee table
+            db.query(`INSERT INTO employees (first_name, last_name, role_id, manager) 
+            VALUE   (${data.firstName}),
+                    (${data.lastName}),
+                    (${data.role}),
+                    (${data.manager});`);
+        })
+};
+
 
 const init = () => {
     inquirer
@@ -38,70 +137,3 @@ const init = () => {
             }
         });
 };
-
-const addDepartment = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is the name of the department?',
-                name: 'department',
-            }
-        ])
-        .then((data) => {
-            // Add department to department table.
-        });
-};
-
-const addRole = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is the name of the role?',
-                name: 'title',
-            },
-            {
-                type: 'input',
-                message: 'What is the salary of the role?',
-                name: 'salary',
-            },
-            {
-                type: 'list',
-                message: 'Which department does the role belong to?',
-                name: 'department',
-                choices: [...departments], 
-            }
-        ])
-        .then((data) => {
-            // Add role to roles table
-        });
-
-};
-
-const addEmployee = () => {
-    inquirer
-        prompt([
-            {
-                type: 'input',
-                message: "What is the employee's first name?",
-                name: 'firstName',
-            },
-            {
-                type: 'input',
-                message: "What is the employee's last name?",
-                name: 'lastName',
-            },
-            {
-                type: 'list',
-                message: "What is the employee's role?",
-                name: 'role',
-                choices: [...roles],
-            }
-        ])
-        .then((data) => {
-            // Add employee data to employee table
-        })
-};
-
-
