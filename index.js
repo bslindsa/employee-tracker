1fd545d12095978ed6ed9b95a37088afb062513e
Brad Lindsay <bslindsa@gmail.com>
2022-01-18 19:19:46 -0700

    Fixed queries and updated schema to fit with desired criteria (manager_id is a int)

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const cTable = require('console.table');
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: 'CSanderson1!',
-      database: 'employees_db'
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'CSanderson1!',
+        database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
-  );
+);
 
 const viewDepartments = () => {
     db.query(`SELECT * FROM employees_db.departments`, (err, result) => {
         console.table(result);
+        //ask question again 
+        init();
     });
+
 };
 
 const viewRoles = () => {
@@ -26,16 +29,23 @@ const viewRoles = () => {
     JOIN departments ON roles.department_id = departments.id
     ORDER BY id ASC;`, (err, result) => {
         console.table(result);
+        //ask question again 
+        init();
     });
+
 };
 
 const viewEmployees = () => {
-    db.query(`SELECT employees.id, first_name, last_name, title, departments.department, salary, manager FROM employees
+    db.query(`SELECT employees.id, employees.first_name, employees.last_name, title, departments.department, salary, emp.first_name as 'Manager ' FROM employees
+    LEFT JOIN employees emp ON emp.id = employees.manager_id
     JOIN roles ON employees.role_id = roles.id
-    JOIN departments ON roles.department_id = departments.id
+    JOIN departments ON roles.department_id = departments.id   
     ORDER BY id ASC;`, (err, result) => {
         console.table(result);
+        //ask question again 
+        init();
     });
+
 };
 
 const addDepartment = () => {
@@ -49,85 +59,134 @@ const addDepartment = () => {
         ])
         .then((data) => {
             // Add department to department table.
+            //`INSERT INTO departments SET ? `, {department: data.department}
             db.query(`INSERT INTO departments (department) 
-                VALUE (${data.department});`, (err, result) => {
-                    console.log(result);
-                });
+            VALUES  ('${data.department}');`, (err, result) => {
+                console.log("err", err);
+                console.log(result);
+                //ask question again
+                init();
+            });
         });
 };
 
 const addRole = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is the name of the role?',
-                name: 'title',
-            },
-            {
-                type: 'input',
-                message: 'What is the salary of the role?',
-                name: 'salary',
-            },
-            {
-                type: 'list',
-                message: 'Which department does the role belong to?',
-                name: 'department',
-                choices: [], 
-            }
-        ])
-        .then((data) => {
-            // Add role to roles table
-            db.query(`INSERT INTO roles (title, salary, department_id) 
-                VALUE   (${data.title}),
-                        (${data.salary}),
-                        (${data.department});`);
-        });
+
+    db.query('SELECT * FROM departments', (req, res) => {
+        const departList = res.map((item, i) => ({
+            name: item.department,
+            value: item.id
+        }));
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What is the name of the role?',
+                    name: 'title',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the salary of the role?',
+                    name: 'salary',
+                },
+                {
+                    type: 'list',
+                    message: 'Which department does the role belong to?',
+                    name: 'department_id',
+                    choices: departList,
+                }
+            ])
+            .then((data) => {
+                // Add role to roles table
+                db.query(`INSERT INTO roles (title, salary, department_id) 
+                VALUE   ('${data.title}',
+                        ${data.salary},
+                        ${data.department_id});`, (err, res) => {
+
+                    if (err) console.log(err);
+
+                    console.table(res);
+                    //ask question again 
+                    init();
+                });
+            });
+    })
+
 
 };
 
 const addEmployee = () => {
-    // db.query('SELECT title, manager FROM employees JOIN roles ON employees.role_id = roles.id', (req, res) => {
+    db.query('SELECT title, id FROM roles', (req, res) => {
+        const roleList = res.map((item, i) => ({
+            name: item.title,
+            value: item.id
+        }));
+        db.query('SELECT id, first_name, last_name FROM employees', (req, empRes) => {
+            const empList = empRes.map((item, i) => ({
+                name: `${item.first_name} ${item.last_name}`,
+                value: item.id
+            }));
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: "What is the employee's first name?",
+                        name: 'firstName',
+                    },
+                    {
+                        type: 'input',
+                        message: "What is the employee's last name?",
+                        name: 'lastName',
+                    },
+                    {
+                        type: 'list',
+                        message: "What is the employee's role?",
+                        name: 'role',
+                        choices: roleList,
+                    },
+                    {
+                        type: 'list',
+                        message: "Who is the employee's manager?",
+                        name: 'manager',
+                        choices: empList
+                    }
+                ])
+                .then((data) => {
+                    // Add employee data to employee table
+                    db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+                VALUE   ('${data.firstName}',
+                        '${data.lastName}',
+                        '${data.role}',
+                        ${data.manager});`, (err, data) => {
+                        console.log(data);
+                        init();
+                    });
+                }) //end of inquirer.prompt 
+        }); //end of role query 
+    }); //end of employee query 
+};
 
-    // })
+const updateEmployee = () => {
     inquirer
         .prompt([
             {
-                type: 'input',
-                message: "What is the employee's first name?",
-                name: 'firstName',
-            },
-            {
-                type: 'input',
-                message: "What is the employee's last name?",
-                name: 'lastName',
+                type: 'list',
+                message: "Which employee's role would you like to update?",
+                name: 'employee',
+                choices: []
             },
             {
                 type: 'list',
-                message: "What is the employee's role?",
+                message: "Which role do you want to assign the selected employee?",
                 name: 'role',
-                choices: [],
-            },
-            {
-                type: 'list',
-                message: "Who is the employee's manager?",
-                name: 'manager',
                 choices: []
             }
         ])
         .then((data) => {
-            // Add employee data to employee table
-            db.query(`INSERT INTO employees (first_name, last_name, role_id, manager) 
-                VALUE   (${data.firstName}),
-                        (${data.lastName}),
-                        (${data.role}),
-                        (${data.manager});`, (err, data) => {
-                console.log(data);    
-                });
+            db.query('UPDATE employees SET title = ? WHERE first_name = ?', data.role, data.employee)
         })
-};
+}
 
-// Use console.table() to make formatted tables
 const init = () => {
     inquirer
         .prompt([
@@ -166,9 +225,12 @@ const init = () => {
                     break;
                 case 'Update an employee role':
                     // Prompt to select an employee to update and their new role and update in the database.
+                    updateEmployee();
                     break;
                 default:
-                    return;
+                    console.log('Exiting application');
+                    process.exit(0);
+
             }
         });
 };
--- seeds.sql
+++ seeds.sql
@@ -17,12 +17,12 @@ VALUES   ('Sales Lead', 4, 100000),
         ('Lawyer', 3, 190000);
 
 -- Add to Employees
-INSERT INTO employees (first_name, last_name, role_id, manager)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES  ('John', 'Doe', 1, null),
-        ('Mike', 'Chan', 2, 'John Doe'),
+        ('Mike', 'Chan', 2, 1),
         ('Ashley', 'Rodriguez', 3, null),
-        ('Kevin', 'Tupik', 4, 'Ashley Rodgriquez'),
+        ('Kevin', 'Tupik', 4, 3),
         ('Kunal', 'Singh', 5, null),
-        ('Malia', 'Brown', 6, 'Kunal Singh'),
+        ('Malia', 'Brown', 6, 5),
         ('Sarah', 'Lourd', 7, null),
-        ('Tom', 'Allen', 8, 'Sarah Lourd');
+        ('Tom', 'Allen', 8, 7);
